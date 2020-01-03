09c361f48b4917fe3770c6a2014682f76c04fb24
shuyang0129 <shuyang0129@gmail.com>
2020-01-04 00:33:50 +0800

    Remove unnecessary files

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,14 @@
 <template>
     <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png" class="mx-auto" />
-        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-        <button
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-        >
-            Button
-        </button>
+        <router-view></router-view>
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
 
 @Component({
-    components: {
-        HelloWorld,
-    },
+    components: {},
 })
 export default class App extends Vue {}
 </script>
-
-<style lang="scss">
-#app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
-</style>
