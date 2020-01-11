6fd0f5c7edbafee873dcf33fe9cc48523bf8546c
shuyang <shuyang0129@gmail.com>
2020-01-11 10:44:51 +0800

    Create sidebar component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="h-full">
         <Header />
-        <div class="mt-16">
+        <div class="relative mt-20 lg:flex lg:items-start">
             <MovieCard />
+            <Sidebar />
         </div>
     </div>
 </template>
@@ -11,11 +12,13 @@
 import { Component, Vue } from 'vue-property-decorator';
 import MovieCard from '@/components/MovieCard.vue';
 import Header from '@/components/Header/Header.vue';
+import Sidebar from '@/components/Sidebar/Sidebar.vue';
 
 @Component({
     components: {
         MovieCard,
         Header,
+        Sidebar,
     },
 })
 export default class Home extends Vue {}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,30 @@
 module.exports = {
     theme: {
-        extend: {},
+        extend: {
+            customForms: theme => ({
+                default: {
+                    input: {
+                        backgroundColor: theme('colors.gray.700'),
+                        borderStyle: 'none',
+                        color: theme('colors.gray.200'),
+                        fontSize: theme('fontSize.sm'),
+                    },
+                    select: {
+                        backgroundColor: theme('colors.gray.700'),
+                        borderStyle: 'none',
+                        color: theme('colors.gray.200'),
+                        fontSize: theme('fontSize.sm'),
+                    },
+                    radio: {
+                        color: theme('colors.gray.600'),
+                    },
+                    checkbox: {
+                        color: theme('colors.gray.700'),
+                    },
+                },
+            }),
+        },
     },
     variants: {},
-    plugins: [],
+    plugins: [require('@tailwindcss/custom-forms')],
 };
